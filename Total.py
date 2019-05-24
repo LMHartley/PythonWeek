e25ac5666faaf1120273c249c730f9f38c410524
LMHartley <liamhartley321@live.co.uk>
2019-05-24 10:11:37 +0100

    PW day 1

---
+++ Total.py
@@ -0,0 +1,12 @@
+phy=80
+che=100
+ma=100
+total=phy+che+ma
+per=total*100/150
+print("physics:",phy)
+print("chemistry:",che)
+print("maths:",ma)
+print("--------------------")
+print("total marks:",total)
+print("percentage:",per)
+print("--------------------")
