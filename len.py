6a228e9fe6a36965215f29e6df7afe498527c792
LMHartley <liamhartley321@live.co.uk>
2019-05-28 11:58:49 +0100

    loops

---
+++ len.py
@@ -0,0 +1,5 @@
+msg=input("enter message here:")
+i=0
+while i<len(msg):
+    print(msg[i])
+    i=i+1
---
+++ nestedloop.py
@@ -0,0 +1,8 @@
+a=int(input("enter starting number:"))
+lim=int(input("enter limit:"))
+while a<=lim:
+    b=1
+    while b<=a:
+        print(a)
+        b=b+1
+    a=a+1
