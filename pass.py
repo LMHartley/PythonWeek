7538d781dae177aa6bfab244e33a0cede80f3f4c
LMHartley <liamhartley321@live.co.uk>
2019-05-28 09:02:16 +0100

    24/05/2019

---
+++ pass.py
@@ -0,0 +1,5 @@
+user=input("user:")
+pw=input("password:")
+if pw==5630:
+    print("yes")
+
--- password.py
+++ password.py
@@ -1,30 +1,18 @@
-user=input("enter username:")
+user=input("username:")
 import getpass
-getpass.getpass("enter password:")
+import sys
+p=getpass.getpass(stream=sys.stderr)
+
 import time
 import sys
-
-
 animation = "|/-\\"
-
 for i in range(10):
     time.sleep(0.1)
     sys.stdout.write("\r" + animation[i % len(animation)])
     sys.stdout.flush()
 print("                       ")
-if getpass==5630 and user=="Liam":
-    print("--------------------")
-    print("access granted")
-    print("--------------------")
-if getpass!=5630 and user!="Liam":
-    print("--------------------")
-    print("access denied")
-    print("--------------------")
-if getpass==5630 and user!="Liam":
-    print("--------------------")
-    print("access denied")
-    print("--------------------")
-if getpass!=5630 and user=="Liam":
-    print("--------------------")
-    print("access denied")
-    print("--------------------")
+
+if p == 5630:
+    print("yes")
+
+
