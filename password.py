137907929ff292ec5a9b09e6e2db25bfe47b4051
LMHartley <liamhartley321@live.co.uk>
2019-05-28 16:57:37 +0100

    28/05/2019 16:57

--- password.py
+++ password.py
@@ -1,18 +1,23 @@
-user=input("username:")
-import getpass
-import sys
-p=getpass.getpass(stream=sys.stderr)
-
-import time
-import sys
-animation = "|/-\\"
-for i in range(10):
-    time.sleep(0.1)
-    sys.stdout.write("\r" + animation[i % len(animation)])
-    sys.stdout.flush()
-print("                       ")
-
-if p == 5630:
-    print("yes")
-
-
+access="no"
+limit=0
+while access=="no":
+    user=input("username:")
+    import getpass
+    import sys
+    p=int(getpass.getpass(stream=sys.stderr))
+    import time
+    import sys
+    animation = "|/-\\"
+    for i in range(10):
+        time.sleep(0.1)
+        sys.stdout.write("\r" + animation[i % len(animation)])
+        sys.stdout.flush()
+    if p==5630 and user=="Liam":
+        print("access granted")
+        access="yes"
+    else:
+        print("access denied")
+        limit=limit+1
+    if limit==3:
+        access="locked out"
+        print("locked out")
