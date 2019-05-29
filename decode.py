741d8576b6f4bcaa59bcc199fed27c1d8e13906b
LMHartley <liamhartley321@live.co.uk>
2019-05-29 14:56:50 +0100

    29/05/2019 14:56

--- decode.py
+++ decode.py
@@ -1,19 +1,17 @@
 msg=input("enter msg here:")
-def change(msg)
-    i=0
-    value=""
-    while i<len(msg):
-        msgi=ord(msg[i])
-        if msgi>=65 and msgi<=90:
-            value+=chr(msgi+32)
+i=0
+value=""
+while i<len(msg):
+    msgi=ord(msg[i])
+    if msgi>=65 and msgi<=90:
+        value+=chr(msgi+32)
+    else:
+        if msgi>=97 and msgi<=122:
+            value+=chr(msgi-32)
         else:
-            if msgi>=97 and msgi<=122:
-                value+=chr(msgi-32)
+            if msgi>=48 and msgi<=57:
+                value+=str(int(chr(msgi))*2)
             else:
-                if msgi>=48 and msgi<=57:
-                    value+=str(int(chr(msgi))*2)
-                else:
-                    value+="*"
-        i=i+1
-    return value
+                value+="*"
+    i=i+1
 print(value)
